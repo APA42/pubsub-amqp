4e0d41279382bd51a275c81f1ae794617f8782f6
Néstor Salceda <nestor.salceda@gmail.com>
2017-09-24 12:44:50 +0200

    Create skeleton for pubsub-amqp gem

---
+++ lib/pubsub/amqp.rb
@@ -0,0 +1,7 @@
+require "pubsub/amqp/version"
+
+module Pubsub
+  module Amqp
+    # Your code goes here...
+  end
+end
---
+++ lib/pubsub/amqp/version.rb
@@ -0,0 +1,5 @@
+module Pubsub
+  module Amqp
+    VERSION = "0.1.0"
+  end
+end
